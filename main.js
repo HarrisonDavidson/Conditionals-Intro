455cab2697e1f1bbddc3e0f70967ce78bc8faa67
Harrison Davidson <hossdavidson@gmail.com>
2023-02-15 13:29:18 -0500

    Conditionals Intro

---
+++ main.js
@@ -0,0 +1,53 @@
+// // Exercise 1
+//  if (1+1>5){
+//     console.log("True fact!");
+// } else {
+//     console.log("LIES!");
+// }
+
+// if (4*5<=20){
+//     console.log("True fact!");
+// } else {
+//     console.log("LIES!");
+// }
+
+// if (6-2>=0){
+//     console.log("True fact!");
+// } else {
+//     console.log("LIES!");
+// }
+
+// // Exercise 2
+// var likesDogs = true
+// if (likesDogs = true){
+//     console.log("You're a dog person!");
+// } else {
+//     console.log("You're a cat person!");
+// }
+
+// // Exercise 3
+// const person = {
+//     name: "Fred",
+//     language: "French"
+// }
+// if (person.language === "English"){
+//     console.log(`Hello, ${person.name}!`);
+// } else if (person.language === "Spanish"){
+//     console.log(`Hola, ${person.name}!`);
+// } else if (person.language === "French"){
+//     console.log(`Bonjour, ${person.name}!`);
+// }
+
+// // // Exercise 4
+// // var grade = 94
+// // if(grade>=0 && grade<=69){
+// //     console.log("You got an F");
+// // } else if (grade>=70 && grade<76){
+// //         console.log("You got a D");
+// // } else if(grade>=77 && grade<84){
+// //     console.log("You got a C");
+// // } else if (grade>=84 && grade<92){
+// //     console.log("You got a B");
+// // } else if (grade>=93 && grade<100){
+// //     console.log("You got an A");
+// // }
